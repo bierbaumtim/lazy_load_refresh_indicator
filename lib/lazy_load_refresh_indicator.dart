fe4e1c60719b8c22c10628040abed102c75e46fb
bierbaumtim <tb2706@outlook.de>
2021-03-13 17:34:42 +0100

    Migrate to nnbd and expose all parameters from RefreshIndicator

--- lib/lazy_load_refresh_indicator.dart
+++ lib/lazy_load_refresh_indicator.dart
@@ -10,6 +10,21 @@ import 'package:flutter/material.dart';
 /// Used by [LazyLoadRefreshIndicator.onRefresh].
 typedef RefreshCallback = Future<void> Function();
 
+/// A widget that supports the Material "swipe to refresh" idiom and lazy loading.
+///
+/// When the child's [Scrollable] descendant overscrolls at the top, an animated circular
+/// progress indicator is faded into view. When the scroll ends, if the
+/// indicator has been dragged far enough for it to become completely opaque,
+/// the [onRefresh] callback is called. The callback is expected to update the
+/// scrollable's contents and then complete the [Future] it returns. The refresh
+/// indicator disappears after the callback's [Future] has completed.
+///
+/// The trigger mode is configured by [RefreshIndicator.triggerMode].
+///
+/// When the child's [Scrollable] descendant overscrolls at the end and the 
+/// overscroll is higher than the [scrollOffset] the [onEndOfPage] callback is called.
+///
+/// A [LazyLoadRefreshIndicator] can only be used with a vertical scroll view.
 class LazyLoadRefreshIndicator extends StatefulWidget {
   /// The offset to take into account when triggering [onEndOfPage] in pixels
   final int scrollOffset;
@@ -33,17 +48,92 @@ class LazyLoadRefreshIndicator extends StatefulWidget {
   /// Typically a [ListView] or [CustomScrollView].
   final Widget child;
 
+  /// The distance from the child's top or bottom edge to where the refresh
+  /// indicator will settle. During the drag that exposes the refresh indicator,
+  /// its actual displacement may significantly exceed this value.
+  /// 
+  /// Exposed from [RefreshIndicator]
+  final double displacement;
+
+  /// The progress indicator's foreground color. The current theme's
+  /// [ThemeData.accentColor] by default.
+  /// 
+  /// Exposed from [RefreshIndicator]
+  final Color? color;
+
+  /// The progress indicator's background color. The current theme's
+  /// [ThemeData.canvasColor] by default.
+  /// 
+  /// Exposed from [RefreshIndicator]
+  final Color? backgroundColor;
+
+  /// A check that specifies whether a [ScrollNotification] should be
+  /// handled by this widget.
+  ///
+  /// By default, checks whether `notification.depth == 0`. Set it to something
+  /// else for more complicated layouts.
+  /// 
+  /// Exposed from [RefreshIndicator]
+  final ScrollNotificationPredicate notificationPredicate;
+
+  /// {@macro flutter.progress_indicator.ProgressIndicator.semanticsLabel}
+  ///
+  /// This will be defaulted to [MaterialLocalizations.refreshIndicatorSemanticLabel]
+  /// if it is null.
+  /// 
+  /// Exposed from [RefreshIndicator]
+  final String? semanticsLabel;
+
+  /// {@macro flutter.progress_indicator.ProgressIndicator.semanticsValue}
+  /// 
+  /// Exposed from [RefreshIndicator]
+  final String? semanticsValue;
+
+  /// Defines `strokeWidth` for `RefreshIndicator`.
+  ///
+  /// By default, the value of `strokeWidth` is 2.0 pixels.
+  /// 
+  /// Exposed from [RefreshIndicator]
+  final double strokeWidth;
+
+  /// Defines how this [RefreshIndicator] can be triggered when users overscroll.
+  ///
+  /// The [RefreshIndicator] can be pulled out in two cases,
+  /// 1, Keep dragging if the scrollable widget at the edge with zero scroll position
+  ///    when the drag starts.
+  /// 2, Keep dragging after overscroll occurs if the scrollable widget has
+  ///    a non-zero scroll position when the drag starts.
+  ///
+  /// If this is [RefreshIndicatorTriggerMode.anywhere], both of the cases above can be triggered.
+  ///
+  /// If this is [RefreshIndicatorTriggerMode.onEdge], only case 1 can be triggered.
+  ///
+  /// Defaults to [RefreshIndicatorTriggerMode.onEdge].
+  /// 
+  /// Exposed from [RefreshIndicator]
+  final RefreshIndicatorTriggerMode triggerMode;
+
+  /// Creates a lazy load refresh indicator.
+  ///
+  /// The [child], [onEndOfPage] and [onRefresh] arguments must be non-null.
+  /// The default [scrollOffset] is 100 logical pixels, the default [displacement] is 40 logical pixels
+  /// and [isLoading] is false.
   const LazyLoadRefreshIndicator({
-    Key key,
+    Key? key,
+    required this.child,
+    required this.onEndOfPage,
+    required this.onRefresh,
     this.scrollOffset = 100,
-    @required this.onEndOfPage,
     this.isLoading = false,
-    @required this.onRefresh,
-    @required this.child,
-  })  : assert(child != null),
-        assert(onRefresh != null),
-        assert(onEndOfPage != null),
-        super(key: key);
+    this.displacement = 40.0,
+    this.color,
+    this.backgroundColor,
+    this.notificationPredicate = defaultScrollNotificationPredicate,
+    this.semanticsLabel,
+    this.semanticsValue,
+    this.strokeWidth = 2.0,
+    this.triggerMode = RefreshIndicatorTriggerMode.onEdge,
+  }) : super(key: key);
 
   @override
   _LazyLoadRefreshIndicatorState createState() =>
@@ -51,7 +141,7 @@ class LazyLoadRefreshIndicator extends StatefulWidget {
 }
 
 class _LazyLoadRefreshIndicatorState extends State<LazyLoadRefreshIndicator> {
-  bool _isLoading;
+  late bool _isLoading;
 
   @override
   void initState() {
@@ -68,11 +158,19 @@ class _LazyLoadRefreshIndicatorState extends State<LazyLoadRefreshIndicator> {
   @override
   Widget build(BuildContext context) {
     return NotificationListener<ScrollNotification>(
-      child: RefreshIndicator(
-        child: widget.child,
-        onRefresh: widget.onRefresh,
-      ),
       onNotification: _handleLoadMoreScroll,
+      child: RefreshIndicator(
+        onRefresh: widget.onRefresh,
+        backgroundColor: widget.backgroundColor,
+        color: widget.color,
+        displacement: widget.displacement,
+        notificationPredicate: widget.notificationPredicate,
+        strokeWidth: widget.strokeWidth,
+        triggerMode: widget.triggerMode,
+        semanticsLabel: widget.semanticsLabel,
+        semanticsValue: widget.semanticsValue,
+        child: widget.child,
+      ),
     );
   }
 
